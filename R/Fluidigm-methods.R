189376393606d8c478f22a817366bbf828b27ba4
Greg Finak <gfinak@fhcrc.org>
2013-02-12 10:00:44 -0800

    Fixed bug in filter that caused erroneous parsing of input argument filt_control, and added require(plyr) to vignette since is.formula was not being located despite import in the namespace

--- R/Fluidigm-methods.R
+++ R/Fluidigm-methods.R
@@ -195,7 +195,7 @@ filter <- function(sc, groups=NULL, filt_control=NULL, apply_filter=TRUE){
   }
     #if filt_control$filter==TRUE & apply_filter==FALSE
     #then rename the retured rows using the wellKey mapping
-    if(filt_control$filter==TRUE&apply_filter==FALSE){
+    if(ifelse(exists("filter",filt_control),get("filter",filt_control),TRUE)&apply_filter==FALSE){
       wk<-as.list(names(getwellKey(sc)))
       names(wk)<-getwellKey(sc)
       rownames(scout)<-data.frame(idvars=do.call(rbind,wk[rownames(scout)]))[,1]
