e611b64bacc33bb008cd76c6ffbc368e42986ea6
Greg Finak <gfinak@fhcrc.org>
2013-02-13 10:23:41 -0800

    Added default Cmax=40 argument to computeEtFromCt, can be changed if fewer cylces are performed. Rebuilt docs

--- R/helper-methods.R
+++ R/helper-methods.R
@@ -4,10 +4,11 @@
 ##' @title computeEtFromCt
 ##' @param df a \code{data.frame}
 ##' @param column The name of the \code{Ct} column. A \code{character}. 'Ct' by default.
+##' @param Cmax the maximum number of cycles performed. 40 by default.
 ##' @return A copy of \code{df} with the 'Et' column appended
 ##' @author Greg Finak
 ##' @export computeEtFromCt
 ##' @aliases computeEtFromCt
-computeEtFromCt<-function(df,column='Ct'){
-  within.data.frame(df, {Et <- 40-get(column); Et <- ifelse(is.na(Et), 0, Et)})
+computeEtFromCt<-function(df,column='Ct',Cmax=40){
+  within.data.frame(df, {Et <- Cmax-get(column); Et <- ifelse(is.na(Et), 0, Et)})
 }
