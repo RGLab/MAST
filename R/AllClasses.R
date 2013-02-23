2b7408f6e641e63886f666572d3c4e59b51dc6a9
Greg Finak <gfinak@fhcrc.org>
2013-02-23 09:33:14 -0800

    Closes #7

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -401,8 +401,13 @@ SingleCellAssay<-function(dataframe=NULL,idvars=NULL,primerid=NULL,measurement=N
   #if geneid == primerid make a new primerid column ensuring it is unique
   if(geneid==primerid|is.null(primerid)){
     #creates a new column called primerid
-    dataframe<-ddply(dataframe,idvars,transform,primerid=make.unique(as.character(GeneID)))
+    mkunique<-function(x,G){
+      cbind(x,primerid=make.unique(as.character(get(G,x))))
+    }
+    dataframe<-ddply(dataframe,idvars,mkunique,G=geneid)
     primerid<-"primerid"
+  }else{
+    ddply(dataframe,idvars,mkunique,G=primerid)
   }
   env<-new.env()
   if(!is.null(mapping)){
