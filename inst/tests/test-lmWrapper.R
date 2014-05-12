108eeda6535ad3754c4329b93af6d38e8347f8c4
Andrew McDavid <amcdavid@fhcrc.org>
2014-05-12 14:56:28 -0700

    Initial commit

---
+++ inst/tests/test-lmWrapper.R
@@ -0,0 +1,30 @@
+source('common-fixtures.R')
+T <- new('GLMlike', design=cData(fd), formula=~Stim.Condition+ncells)
+context('Testing GLMLike construction')
+test_that('Can construct GLMLike', {
+    expect_is(T, 'GLMlike')
+})
+
+T <- fit(T, response=exprs(fd)[,2])
+test_that('Can fit', {
+    expect_is(coefC(T), 'numeric')
+    expect_is(coefD(T), 'numeric')
+    
+})
+
+test_that('Design is invariant to updates', {
+        T2 <- update(T, ~ Stim.Condition)
+        expect_true(length(T2@fitC)==0)
+        expect_equivalent(T2@design, T@design)
+        expect_true(length(setdiff(colnames(T@modelMatrix), colnames(T2@modelMatrix)))==1)
+        expect_true(length(setdiff(colnames(T2@modelMatrix), colnames(T@modelMatrix)))==0)
+})
+
+
+test_that('log likelihood is increasing in model complexity', {
+    l1 <- logLik(T)
+    T2 <- update(T, ~ Stim.Condition)
+    T2 <- fit(T2)
+    l0 <- logLik(T2)
+    expect_true(l0<l1)
+})
