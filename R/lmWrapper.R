d900e377d5c5be59a0d5fe80d60dccc930efa851
Andrew McDavid <amcdavid@fhcrc.org>
2014-06-09 18:28:58 -0700

    documentation for new zlm code

--- R/lmWrapper.R
+++ R/lmWrapper.R
@@ -1,15 +1,94 @@
 ## Generics
+
+##' fit a zero-inflated regression
+##'
+##' Given a design and formula, fit the zero inflated regression, storing the fits in slots
+##' \code{fitC} and \code{fitD}
+##' @param object inheriting from \code{LMlike}
+##' @param response a vector, same length as the design, or if missing then use the current response
+##' @param ... currently ignored
+##' @return LMlike or subclass
+##' @export
 setGeneric('fit', function(object, response, ...) standardGeneric('fit'))
+
+##' Coefficients of zero-infated
+##'
+##' Given a fitted LMlike, return the coefficients from discrete or continuous
+##' @param object LMlike
+##' @param which  character vector, one of "C" (continuous) or "D" (discrete) specifying which component should be returned
+##' @param ... passed to methods
+##' @return numeric vector
+##' @export
 setGeneric('coef', function(object, ...) standardGeneric('coef'))
 #setGeneric('coefD', function(object) standardGeneric('coefD'))
+
+##' Run a likelihood-ratio test
+##'
+##' Compares the change in likelihood between the current \code{formula} and one dropping terms in \code{drop.terms}.
+##' Only complete terms can be tested at this time
+##' @param object LMlike or subclass
+##' @param drop.terms character vector of \code{formula} terms
+##' @return array giving test statistics
+##' @export
+##' @seealso fit
+##' @seealso waldTest
 setGeneric('lrTest', function(object, drop.terms) standardGeneric('lrTest'))
+
+##' Run a Wald test
+##'
+##' Run a Wald test
+##' @param object LMlike or subclass
+##' @param hypothesis.matrix argument suitable to be passed to car::lht
+##' @return array giving test statistics
+##' @export
+##' @seealso fit
+##' @seealso lrTest
 setGeneric('waldTest', function(object, hypothesis.matrix) standardGeneric('waldTest'))
 ## setGeneric('vcovC', function(object) standardGeneric('vcovC'))
 ## setGeneric('vcovD', function(object) standardGeneric('vcovD'))
+
+##' Variance-covariance matrix for zero inflated
+##'
+##' Given a fitted LMlike, return the variance-covariance from discrete or continuous
+##' @param object LMlike
+##' @param which character vector, one of "C" (continuous) or "D" (discrete) specifying which component should be returned
+##' @return matrix
+##' @export
 setGeneric('vcov', function(object) standardGeneric('vcov'))
+
+##' Degrees of freedom of Zero inflated model
+##'
+##' @param object LMlike or subclass
+##' @return vector giving the model degrees of freedom for continuous and discrete
+##' @export
 setGeneric('dof', function(object) standardGeneric('dof'))
 
 ## Classes
+##' Linear Model-like Class
+##'
+##' Wrapper around modeling function to make them behave enough alike that Wald tests and Likelihood ratio are easy to do.
+##' To implement a new type of zero-inflated model, extend this class.
+##'
+##' @section Slots:
+##' \describe{
+##' \item{design}{a data.frame from which variables are taken for the right hand side of the regression}
+##' \item{fitC}{The continuous fit}
+##' \item{fitD}{The discrete fit}
+##' \item{response}{The left hand side of the regression}
+##' \item{fitted}{A \code{logical} with components "C" and "D", TRUE if the respective component has converge}
+##' \item{formula}{A \code{formula} for the regression}
+##' \item{fitArgsC}{}
+##' \item{fitArgsD}{Both \code{list}s giving arguments that will be passed to the fitter (such as convergence criteria or case weights)}
+##' }
+##' @seealso fit
+##' @seealso coef
+##' @seealso lrTest
+##' @seealso waldTest
+##' @seealso vcov
+##' @seealso dof
+##' @seealso logLik
+##' @name LMlike-class
+##' @docType class
 setClass('LMlike', slots=c(design='data.frame', fitC='ANY', fitD='ANY', response='ANY', fitted='logical', formula='formula', fitArgsD='list', fitArgsC='list'),     prototype=list(fitted =c(C=FALSE, D=FALSE)), validity=function(object){
     stopifnot( all(c("C", "D") %in% names(object@fitted)))
     if(length(object@response)>0 && any(is.na(object@response))) stop('NAs not permitted in response')
@@ -88,7 +167,7 @@ makeChiSqTable <- function(lambda, df, test){
     lambdaC <- c(lambda, sum(lambda))
     dfC <- c(df, sum(df))
     tab <- cbind(lambda=lambdaC,
-               df=dfC, 'P(Chisq>lambda)'=pchisq(lambdaC, df=dfC, lower.tail=FALSE))
+               df=dfC, 'Pr(>Chisq)'=pchisq(lambdaC, df=dfC, lower.tail=FALSE))
     row.names(tab) <- c('cont', 'disc', 'hurdle')
     structure(tab, test=test)
 }
