6c4de80c385f610108aa754d569459f375f9fda7
Andrew McDavid <amcdavid@fhcrc.org>
2014-07-12 22:41:34 -0700

    forgot file with method definition/bump version

---
+++ R/lmWrapper-ShrunkenGLMlike.R
@@ -0,0 +1,22 @@
+##' @include LmWrapper.R
+##' @include AllClasses.R
+
+
+setMethod('initialize', 'ShrunkenGLMlike', function(.Object, ...){
+    callNextMethod()
+})
+
+
+setMethod('fit', signature=c(object='ShrunkenGLMlike', response='missing'), function(object, response, silent=TRUE, ...){
+    object <- callNextMethod()
+    if(object@fitted['C']){
+    thisdispersion <- object@fitC$dispersionMLE
+    df.total <- object@fitC$df.null+1
+    df.residual <- object@fitC$df.residual
+    object@fitC$dispersionMLEnoshrink <- thisdispersion
+    object@fitC$dispersionMLE <- (thisdispersion*df.total + object@priorVar*object@priorDOF)/(df.total+object@priorDOF)
+    object@fitC$dispersion <- (object@fitC$dispersion*df.residual+object@priorVar*object@priorDOF)/(df.residual+object@priorDOF)
+}
+    object
+    
+})
