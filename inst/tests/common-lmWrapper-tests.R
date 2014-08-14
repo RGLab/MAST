35eb9084b633817f87eb2519da8c4e6964d2f5ba
Andrew McDavid <amcdavid@fhcrc.org>
2014-08-14 13:59:31 -0700

    Fix bug with marginal residuals

--- inst/tests/common-lmWrapper-tests.R
+++ inst/tests/common-lmWrapper-tests.R
@@ -118,6 +118,6 @@ test_that('Wald For Glm', {
 
 
 test_that('Residuals', {
-    expect_equivalent(residuals(obj, which='C', type='response'), residuals(objC, type='response'))
-    expect_equivalent(residuals(obj, which='D', type='deviance'), residuals(objD, type='deviance'))
+    expect_equivalent(as.numeric(residuals(obj, which='C', type='response')), residuals(objC))
+    expect_equivalent(as.numeric(residuals(obj, which='D', type='response')), residuals(objD, type='response'))
 })
