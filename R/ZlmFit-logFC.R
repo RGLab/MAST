812763bb61ef8210d4c4351edad9e37db87b6baa
Andrew McDavid <andrew.n.mcdavid@gmail.com>
2015-03-11 18:47:47 -0700

    logFC can use `Hypothesis` to specify contrasts

--- R/ZlmFit-logFC.R
+++ R/ZlmFit-logFC.R
@@ -19,9 +19,27 @@ safeContrastQF <- function(contrast, vc) uncomplexify(tcrossprod(contrast, compl
 ##' Note that for this to be a log-fold change, then the regression for u must have been fit on the log scale.  This is returned in the matrix \code{logFC}.
 ##' An approximation of the variance of \code{logFC} (applying the delta method to  formula defined above) is provided in \code{varLogFC}.
 ##' @param zlmfit ZlmFit output
-##' @param contrast0 vector of coefficients giving baseline contrast.  If missing, then the '(Intercept)' is used as baseline.
-##' @param contrast1 matrix of coefficients giving comparison contrasts.  If missing, then all non-(Intercept) coefficients are compared.
+##' @param contrast0 vector of coefficients giving baseline contrast, or a \code{\link{Hypothesis}}.  If missing, then the '(Intercept)' is used as baseline.
+##' @param contrast1 matrix of coefficients giving comparison contrasts, or a \code{\link{Hypothesis}}.  If missing, then all non-(Intercept) coefficients are compared.
+##' @seealso Hypothesis
 ##' @return list of matrices `logFC` and `varLogFC`, giving the log-fold-changes for each contrast (columns) and genes (rows) and the estimated sampling variance thereof
+##' @examples
+##' data(vbetaFA)
+##' zz <- zlm.SingleCellAssay( ~ Stim.Condition+Population, vbetaFA[,1:5])
+##' ##log-fold changes in terms of intercept (which is Stim(SEB) and CD154+VbetaResponsive)
+##' lfcStim <- logFC(zz)
+##' ##If we want to compare against unstim, we can try the following
+##' coefnames <- colnames(coef(zz, 'D'))
+##' contrast0 <- setNames(rep(0, length(coefnames)), coefnames)
+##' contrast0[c('(Intercept)', 'Stim.ConditionUnstim')] <- 1
+##' contrast1 <- diag(length(coefnames))
+##' rownames(contrast1)<-colnames(contrast1)<-coefnames
+##' contrast1['(Intercept)',]<-1
+##' lfcUnstim <- logFC(zz, contrast0, contrast1)
+##' ##log-fold change with itself is 0
+##' stopifnot(all(lfcUnstim$logFC[,2]==0))
+##' ##inverse of log-fold change with Stim as reference
+##' stopifnot(all(lfcStim$logFC[,1]==(-lfcUnstim$logFC[,1])))
 ##' @export
 logFC <- function(zlmfit, contrast0, contrast1){
     coname <- colnames(coef(zlmfit, 'D'))
@@ -31,16 +49,21 @@ logFC <- function(zlmfit, contrast0, contrast1){
         contrast0 <- t(as.matrix(setNames(rep(0, length(coname)), coname)))
         contrast0[,'(Intercept)'] <- 1
         rownames(contrast0) <- '(Intercept)'
-    }
+    } else if(inherits(contrast0, 'Hypothesis')){
+        gh <- generateHypothesis(contrast0, coname)
+        contrast0 <- gh@contrastMatrix
+    } 
     if(missing(contrast1)){
         contrast1 <- cbind(0, diag(1, nrow=length(coname)-1))
         colnames(contrast1) <- coname
         rownames(contrast1) <- setdiff(coname, rownames(contrast0))
         ## add contrast0
-        contrast1 <- t(t(contrast1)+as.numeric(contrast0))
-    } else{
-        contrast1 <- t(contrast1)
+        contrast1 <- t(contrast1)+as.numeric(contrast0)
+    } else if(inherits(contrast1, 'Hypothesis')){
+        gh <- generateHypothesis(contrast1, coname)
+        contrast1 <- gh@contrastMatrix
     }
+    contrast1 <- t(contrast1)
     Contr <- rbind(contrast0, contrast1)
     ## Get expectation and variance of contrasts, discrete and continuous
     ## expectation of contrast.  genes x contrast
@@ -69,9 +92,6 @@ logFC <- function(zlmfit, contrast0, contrast1){
     diagIdx <- cbind(diagIdx, k=diagIdx[,'j'])
     dvcont <- matrix(vcont[diagIdx], nrow=length(genes))
     dvdisc <- matrix(vdisc[diagIdx], nrow=length(genes))
-    ## debugging assertion
-    ## stopifnot(all.equal(dvcont[1,], diag(vcont[1,,]), check.attributes=FALSE))
-    ## stopifnot(all.equal(dvdisc[10,], diag(vdisc[10,,]), check.attributes=FALSE))
     ## variance of product is product of variance + higher order terms 
     v.prod <- dvcont*dvdisc + mu.cont^2*dvdisc + mu.disc^2*dvcont
     
@@ -80,9 +100,7 @@ logFC <- function(zlmfit, contrast0, contrast1){
     covc1c0Idx[,'k'] <- 1
     c1c0cont <- matrix(vcont[covc1c0Idx], nrow=length(genes))
     c1c0disc <- matrix(vdisc[covc1c0Idx], nrow=length(genes))
-    ## debugging assertion
-    ## stopifnot(all.equal(c1c0cont[1,], vcont[1,2:nrow(Contr),1], check.attributes=FALSE))
-    ## stopifnot(all.equal(c1c0disc[10,], vdisc[10,2:nrow(Contr),1], check.attributes=FALSE))
+    ## covariance of product is product of covariance + higher order terms
     cov.prod <- c1c0cont*c1c0disc+genewiseMult(mu.cont[,1], mu.cont[,-1,drop=FALSE])*c1c0disc + genewiseMult(mu.disc[,1], mu.disc[,-1,drop=FALSE])*c1c0cont
 
     ## _differences_ between expectation of baseline contrast and all others
