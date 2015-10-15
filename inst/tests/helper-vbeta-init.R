57f95d32fcbc68ef19988df6f7034af8ac3825d7
Andrew McDavid <andrew.n.mcdavid@gmail.com>
2015-10-15 14:19:31 -0700

    intermediate commit to stash changes

---
+++ inst/tests/helper-vbeta-init.R
@@ -0,0 +1,18 @@
+library(testthat)
+
+geneid="Gene"
+primerid='Gene'
+measurement='et'
+idvars=c('Subject.ID', 'Chip.Number', 'Stim.Condition', 'Population', 'Well')
+ncells <- 'Number.of.Cells'
+phenovars=NULL
+cellvars='Experiment.Number'
+featurevars=NULL
+
+##Tests depending on vbeta
+data(vbeta)
+test_that("vbeta can be loaded",{
+  expect_that(vbeta,is_a("data.frame"))
+})
+
+vbeta$et <- ifelse(is.na(vbeta$Ct), 0, 40-vbeta$Ct)
