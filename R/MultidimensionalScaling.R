d4d41eb678b953f338d6c5b99bb2b41c6dc7c89e
Andrew McDavid <amcdavid@fhcrc.org>
2016-05-19 10:29:44 -0700

    makeMM moved to SingleCellAnalysis package

--- R/MultidimensionalScaling.R
+++ R/MultidimensionalScaling.R
@@ -11,21 +11,3 @@ xform <- function(mat, scale=FALSE){
   smat[is.na(smat)] <- 0
   smat
   }
-
-makeMM <- function(FD, type, rescale=TRUE){
-  if(type == 'zeroinf'){
-    return(scale(exprs(FD)))
-  } else if(type == 'hurdle'){
-    return(cbind(xform(exprs(FD), scale=rescale), scale((exprs(FD)>0)*1, scale=rescale)))
-  } else if(type=='dichotomous'){
-    return( scale((exprs(FD[, freq(FD)<1])>0)*1, scale=rescale))
-  } else if(type=='raw'){
-    olayer <- layer(FD)
-    layer(FD) <- 'lCount'
-    model.mat <- scale(exprs(FD), scale=rescale)
-    layer(FD) <- olayer
-    return(model.mat)
-  } else{
-   stop('bad type') 
-  }
-}
